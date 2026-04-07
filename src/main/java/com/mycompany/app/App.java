3062d76624c7ff5420d2cbed412efda449264365
Rosli Shafiee <rosli.shafiee@gmail.com>
2026-04-07 14:46:29 +0800

    Introduced Jenkinsfile

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I love Jenkins");
+        System.out.println("test123");
     }
 
     public String getMessage() {
